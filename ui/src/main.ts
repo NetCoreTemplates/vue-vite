15c553011daa465a4ff42a8099649fdb9db44f8e
Demis Bellot <demis.bellot@gmail.com>
2021-11-29 20:27:59 +0800

    refactor router.ts to only contain common user routes/guards/config

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,12 +1,23 @@
 import "@/styles/index.css"
-import { createApp } from "vue"
 
-import App from "@/App.vue"
-import { router } from "@/router"
+import { createApp } from "vue"
 import { createHead } from "@vueuse/head"
+import { createRouter, createWebHistory } from "vue-router"
+
+import { setupLayouts } from "virtual:generated-layouts"
+import generatedRoutes from "~pages"
+import App from "@/App.vue"
+import { configRouter } from "@/router"
 
 const app = createApp(App)
 const head = createHead()
+
+const routes = setupLayouts(generatedRoutes)
+
+export const router = configRouter(createRouter({
+    history: createWebHistory(),
+    routes,
+}))
 
 app
     .use(head)
--- ui/src/router.ts
+++ ui/src/router.ts
@@ -1,10 +1,6 @@
 import { attrs, loading } from "./auth"
-import { createRouter, createWebHistory, NavigationGuardNext, RouteLocationNormalized, Router } from "vue-router"
+import { NavigationGuardNext, RouteLocationNormalized, Router } from "vue-router"
 import { nextTick, watchEffect } from "vue"
-
-// Auto generated routes by https://github.com/hannoeru/vite-plugin-pages 
-import { setupLayouts } from "virtual:generated-layouts"
-import generatedRoutes from "~pages"
 
 // Typed Routes used in Components
 export const Routes = {
@@ -12,48 +8,45 @@ export const Routes = {
     forbidden: () => '/forbidden',
 }
 
+// Use Route Guards to protect against protected pages 
 type RouteGuard = { path:string, attr:string }
 const ROUTE_GUARDS:RouteGuard[] = [
     { path:'/profile', attr:'auth' },
     { path:'/admin',   attr:'role:Admin' },
-    // Additional guards to protect against 
 ]
 
-const routes = setupLayouts(generatedRoutes)
+export function configRouter(router:Router)  {
+    const invalidAttrRedirect = (to:RouteLocationNormalized, guardAttr:string, userAttrs:string[]) => userAttrs.indexOf('auth') === -1
+        ? Routes.signin(to.path)
+        : Routes.forbidden()
 
-export const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
-
-const invalidAttrRedirect = (to:RouteLocationNormalized, guardAttr:string, userAttrs:string[]) => userAttrs.indexOf('auth') === -1
-    ? Routes.signin(to.path)
-    : Routes.forbidden()
-
-// Validate Route guards against Authenticated User's Attributes
-const validateRoute = (to:RouteLocationNormalized, next:NavigationGuardNext, attrs:string[]) => {
-    for (let i=0; i<ROUTE_GUARDS.length; i++) {
-        const { path, attr } = ROUTE_GUARDS[i]
-        if (!to.path.startsWith(path)) continue
-        if (attrs.indexOf(attr) === -1) {
-            const goTo = invalidAttrRedirect(to, attr, attrs)
-            next(goTo)
-            return
+    // Validate Route guards against Authenticated User's Attributes
+    const validateRoute = (to:RouteLocationNormalized, next:NavigationGuardNext, attrs:string[]) => {
+        for (let i=0; i<ROUTE_GUARDS.length; i++) {
+            const { path, attr } = ROUTE_GUARDS[i]
+            if (!to.path.startsWith(path)) continue
+            if (attrs.indexOf(attr) === -1) {
+                const goTo = invalidAttrRedirect(to, attr, attrs)
+                next(goTo)
+                return
+            }
         }
+        next()
     }
-    next()
-}
 
-router.beforeEach((to,from,next) => {
-    if (loading) {
-        const stop = watchEffect(() => {
+    router.beforeEach((to,from,next) => {
+        if (loading) {
+            const stop = watchEffect(() => {
+                validateRoute(to, next, attrs.value)
+                nextTick(() => stop())
+            })
+        } else {
             validateRoute(to, next, attrs.value)
-            nextTick(() => stop())
-        })
-    } else {
-        validateRoute(to, next, attrs.value)
-    }
-})
+        }
+    })
+
+    return router
+}
 
 export function getRedirect(router:Router) {
     let { redirect } = router.currentRoute?.value.query
