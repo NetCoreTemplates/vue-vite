5dae4215436557238b0a06a86aaef36baf2cfdd1
Demis Bellot <demis.bellot@gmail.com>
2021-11-30 14:04:31 +0800

    center all the pages

--- ui/src/components/AppPage.vue
+++ ui/src/components/AppPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="min-h-screen">
-    <main class="flex justify-center">
-      <div class="mx-auto px-5 mb-32">
+  <div class="min-h-screen mx-auto">
+    <main class="flex">
+      <div class="container px-5 mb-32">
         <AppBreadcrumb class="my-8" :name="title"/>
         <slot></slot>
       </div>
