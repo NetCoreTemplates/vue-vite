33215ae3264a86c19111aa6000cce16b4ee4ffe2
Demis Bellot <demis.bellot@gmail.com>
2021-12-23 20:55:58 +0800

    remove unused EF

--- ui/src/pages/Signin.vue
+++ ui/src/pages/Signin.vue
@@ -1,6 +1,6 @@
 <template>
   <AppPage title="Sign In" class="max-w-xl">
-    
+
     <form @submit.prevent="onSubmit">
       <div class="shadow overflow-hidden sm:rounded-md">
         <ErrorSummary except="userName,password"/>
@@ -26,25 +26,25 @@
     <div class="flex mt-8">
       <h3 class="hidden xs:block mr-4 leading-8 text-gray-500">Quick Links</h3>
       <span class="relative z-0 inline-flex shadow-sm rounded-md">
-      <button type="button" @click="setUser('admin@email.com')"
-              class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-          admin@email.com
-      </button>
-      <button type="button" @click="setUser('manager@email.com')"
-              class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-              manager@email.com
-      </button>
-      <button type="button" @click="setUser('employee@email.com')"
-              class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-              employee@email.com
-      </button>
-      <button type="button" @click="setUser('new@user.com')"
-              class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-              new@user.com
-      </button>
-    </span>
+        <button type="button" @click="setUser('admin@email.com')"
+                class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+            admin@email.com
+        </button>
+        <button type="button" @click="setUser('manager@email.com')"
+                class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+            manager@email.com
+        </button>
+        <button type="button" @click="setUser('employee@email.com')"
+                class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+            employee@email.com
+        </button>
+        <button type="button" @click="setUser('new@user.com')"
+                class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+            new@user.com
+        </button>
+      </span>
     </div>
-    
+
   </AppPage>
 </template>
 
