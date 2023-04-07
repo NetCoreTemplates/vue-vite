92aba1405e5b1731fb534d80c44f2ca6cc04d087
GitHub Action <action@github.com>
2023-04-07 13:17:20 +0000

    deploy: ea217ba43a7975a8e33e1460a887445da572de82

--- assets/empty-35323bb1.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,r as c,o,c as r,a as n}from"./index-ceee07ae.js";const s={},a={class:"h-screen px-4 text-black bg-white text-center flex flex-col items-center justify-center"};function _(i,l){const e=c("router-view");return o(),r("main",a,[n(e)])}const m=t(s,[["render",_]]);export{m as default};
